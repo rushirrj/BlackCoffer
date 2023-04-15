ef7ab608ba0ff7d8fda846cd5af389098df756f1
Rushikesh Jadhav <rushikeshdjrrj@gmail.com>
2023-04-15 18:34:57 +0530

    fixed grid

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -11,22 +11,24 @@ import CountryChart from "../components/Charts/CountryChart";
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-row justify-around">
-          <IntensityChart />
-          <TopicChart />
-          <LikelihoodChart />
-        </div>
-        <div className="flex flex-row justify-around">
-          <YearChart />
-          <RelevanceChart />
-          <EndYearChart />
-        </div>
-        <div className="flex flex-row justify-around">
-          <CountryChart />
-          <RegionChart />
+      <h1 className="font-bold text-center text-xl py-3">Dashboard</h1>
+      <div className="flex flex-col gap-2 flex-wrap">
+        <div className="flex flex-row justify-around flex-wrap gap-2">
+          <div className="shadow-md p-2"><IntensityChart /></div>
+          <div className="shadow-md p-2"><TopicChart /></div>
           
+        </div>
+        <div className="flex flex-row justify-around flex-wrap">
+          <div className=" shadow-md p-2 "><LikelihoodChart /></div>
+          <div className=" shadow-md p-2 "><CountryChart /></div>
+        </div>
+        <div className="flex flex-row justify-around flex-wrap">
+          <div className="shadow-md p-2"><YearChart /></div>
+          <div className="shadow-md p-2"><EndYearChart /></div>
+        </div>
+        <div className="flex flex-row justify-around flex-wrap">
+          <div className="shadow-md p-2"><RegionChart /></div>
+          <div className="shadow-md p-2"><RelevanceChart /></div>
         </div>
       </div>
     </div>
