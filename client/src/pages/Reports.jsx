c36e898d286ad034e6c4961874cce2fbc5a56097
Rushikesh Jadhav <rushikeshdjrrj@gmail.com>
2023-04-15 19:02:21 +0530

    added reportCard and grid

--- client/src/pages/Reports.jsx
+++ client/src/pages/Reports.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReportCard from '../components/Cards/ReportCard'
 const Reports = () => {
   return (
-    <div className='grid grid-cols-3 gap-2'>
-      <div className=''><ReportCard/></div>
+    <div className='grid grid-cols-3 gap-3'>
+    
+        <ReportCard/>
+        <ReportCard/>
+        <ReportCard/>
+        <ReportCard/>
+   
     </div>
   )
 }
