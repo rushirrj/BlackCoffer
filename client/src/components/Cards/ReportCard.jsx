52c9020ae8ee6e34350a3ca982f4894884e4fb7e
ajit <ajitdakre05@gmail.com>
2023-04-15 19:26:10 +0530

    updated UI

--- client/src/components/Cards/ReportCard.jsx
+++ client/src/components/Cards/ReportCard.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 
-const ReportCard = () => {
+const ReportCard = ({
+    content
+}) => {
   return (
     <div>
         <div className='shadow-md flex flex-col p-2 gap-1 rounded-sm'>
-            <div className='font-bold'>Annual Energy Lookout</div>
+            <div className='font-bold'>{content?.insight}</div>
             <div className='flex flex-row gap-2'>
-                <div className='text-sm'>sector</div>
-                <div className='text-sm'>topic</div>
-                <div className='text-sm'>pestle</div>
+                <div className='text-sm'>{content?.sector}</div>
+                <div className='text-sm'>{content?.topic}</div>
+                <div className='text-sm'>{content?.pestle}</div>
             </div>
-            <div className='flex flex-row'>country</div>
+            <div className='flex flex-row'>{content?.country}</div>
             <div className='flex flex-row gap-2'>
-                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>intensity : 3</div>
-                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>likelihood : 2</div>
-                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>relevance : 1</div>
+                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>intensity : {content?.intensity}</div>
+                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>likelihood : {content?.likelihood}</div>
+                <div className='text-xs border-dashed border border-gray-500 p-1 rounded-sm'>relevance : {content?.relevance}</div>
             </div>
-            <div>title</div>
+            <div>{content?.title}</div>
             <div className='flex justify-between'>
-                <div>date</div>
-                <div>source</div>
+                <div>{content?.date}</div>
+                <button onClick={()=>{
+                    window.open(content?.url)
+                }}>URL</button>
             </div>
         </div>
     </div>
--- client/src/pages/Reports.jsx
+++ client/src/pages/Reports.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ReportCard from '../components/Cards/ReportCard'
+import React from "react";
+import ReportCard from "../components/Cards/ReportCard";
 const Reports = () => {
+  const [data, setData] = React.useState([]);
+  const getData = () =>
+    fetch("http://localhost:4000/api/data/")
+      .then((res) => res.json())
+      .then((data) => setData(data?.data));
+  React.useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className='grid grid-cols-3 gap-3'>
-    
-        <ReportCard/>
-        <ReportCard/>
-        <ReportCard/>
-        <ReportCard/>
-   
+    <div className="grid grid-cols-3 gap-3">
+      {data.map((item) => (
+        <ReportCard content={item} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Reports
+export default Reports;
