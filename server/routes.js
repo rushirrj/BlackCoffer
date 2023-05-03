6e3c6de68c662680d770e329d167a051804e9663
ajit <ajitdakre05@gmail.com>
2023-05-03 18:19:42 +0530

    update in api

--- server/routes.js
+++ server/routes.js
@@ -44,9 +44,8 @@ router.get("/data/:id", async (req, res) => {
 
 router.get("/query/:type", async (req, res) => {
   try {
-    // const data = await Data.find();
+    const data = await Data.find();
     const type = req.params.type;
-    const data = await Data.count({ region: "Western Africa" });
     Data.aggregate([
       {
         $group: {
@@ -63,127 +62,18 @@ router.get("/query/:type", async (req, res) => {
     console.log(err);
   }
 });
-
-router.get("/dashboard/intensity", async (req, res) => {
+router.get("/dashboard/:type", async (req, res) => {
   const data = await Data.find();
+  const type = req.params.type;
   let mapData = {};
   mapData[0] = 0;
   data?.forEach((item) => {
-    if (mapData[item.intensity]) {
-      mapData[item.intensity] += 1;
-    } else if (item.intensity === null) {
+    if (mapData[item[type]]) {
+      mapData[item[type]] += 1;
+    } else if (mapData[item[type]] === null) {
       mapData[0] += 1;
     } else {
-      mapData[item.intensity] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/likelihood", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  mapData[0] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.likelihood]) {
-      mapData[item.likelihood] += 1;
-    } else if (item.likelihood === null) {
-      mapData[0] += 1;
-    } else {
-      mapData[item.likelihood] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/relevance", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  mapData[0] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.relevance]) {
-      mapData[item.relevance] += 1;
-    } else if (item.relevance === null) {
-      mapData[0] += 1;
-    } else {
-      mapData[item.relevance] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/start_year", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  // mapData["N.A"] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.start_year]) {
-      mapData[item.start_year] += 1;
-    } else if (item.start_year === null || item.start_year === "") {
-      // mapData["N.A"] += 1;
-    } else {
-      mapData[item.start_year] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/end_year", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  // mapData["N.A"] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.end_year]) {
-      mapData[item.end_year] += 1;
-    } else if (item.end_year === null || item.end_year === "") {
-      // mapData["N.A"] += 1;
-    } else {
-      mapData[item.end_year] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/topic", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  mapData["N.A"] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.topic]) {
-      mapData[item.topic] += 1;
-    } else if (item.topic === null || item.topic === "") {
-      mapData["N.A"] += 1;
-    } else {
-      mapData[item.topic] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/country", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  // mapData["N.A"] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.country]) {
-      mapData[item.country] += 1;
-    } else if (item.country === null || item.country === "") {
-      // mapData["N.A"] += 1;
-    } else if (item.topic == "world") {
-      mapData["World"] += 1;
-    } else {
-      mapData[item.country] = 1;
-    }
-  });
-  return res.status(200).json({ data: mapData, message: "Data fetched" });
-});
-router.get("/dashboard/region", async (req, res) => {
-  const data = await Data.find();
-  let mapData = {};
-  // mapData["N.A"] = 0;
-  data?.forEach((item) => {
-    if (mapData[item.region]) {
-      mapData[item.region] += 1;
-    } else if (item.topic == "world") {
-      mapData["World"] += 1;
-    } else if (item.region === null || item.region === "") {
-      // mapData["N.A"] += 1;
-    } else {
-      mapData[item.region] = 1;
+      mapData[item[type]] = 1;
     }
   });
   return res.status(200).json({ data: mapData, message: "Data fetched" });
